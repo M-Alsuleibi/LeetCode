86729800a6e6fb0d0037e5e42206b622896c0a57
mohamad <201106@ppu.edu.ps>
2024-06-20 01:31:47 +0300

    leetcode problem #1360

---
+++ 1360. Number of Days Between Two Dates/README.md
@@ -0,0 +1,1 @@
+#[1360. Number of Days Between Two Dates](https://leetcode.com/problems/number-of-days-between-two-dates/description/) `EASY`
---
+++ 1360. Number of Days Between Two Dates/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        def isLeap(year):
+	        return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if date1 > date2:
+        	date1, date2 = date2, date1
+        y1, m1, d1 = map(int, date1.split("-"))
+        y2, m2, d2 = map(int, date2.split("-"))
+        result = 0
+        for year in range(y1, y2):
+        	result += 365 + isLeap(year)
+        days[1] = 29 if isLeap(y1) else 28
+        result -= sum(days[ : m1 - 1]) + d1
+        days[1] = 29 if isLeap(y2) else 28
+        result += sum(days[ : m2 - 1]) + d2
+        return result
