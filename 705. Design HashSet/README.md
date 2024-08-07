3f8db727a864078c5ef8f12b5e289ac62d9f58fe
mohamad <201106@ppu.edu.ps>
2024-08-07 18:43:23 +0300

    LeetCode problem #705 modified

--- 705. Design HashSet/README.md
+++ 705. Design HashSet/README.md
@@ -1,2 +1,4 @@
 # [705. Design HashSet](https://leetcode.com/problems/design-hashset/description/) `EASY`
-
+## Runtime and Memory Analyze Complexity:
+- Time complexity : O(1)ForAdd,Remove,Contains
+- Space complexity: O(1)
--- 705. Design HashSet/solution.py
+++ 705. Design HashSet/solution.py
@@ -1,29 +1,28 @@
 # Here I follow chaining approach to build a hashSet
 class ListNode:
-    def __init__(self,key = -1, next = None):
+    def __init__(self,key):
         self.key = key
-        self.next = next
+        self.next = None
 
 class MyHashSet:
 
     def __init__(self):
-        self.map = [ ListNode() for i in range(1000)] # Initialize every Index with a dummy node 
+        self.set = [ ListNode(0) for i in range(10**4)] # Initialize every Index with a dummy node 
 
     # Create this helper Fn bc we use its body in several parts in our code      
     def hash(self, key):
-        return key % len(self.map)
+        return key % len(self.set)
 
     def add(self, key: int) -> None:
-        cur = self.map[self.hash(key)]
+        cur = self.set[self.hash(key)]
         while cur.next: # here .next is to cover pointing in the last node, and in the statr there is a dummy node
             if cur.next.key == key: # If the key is alredy exists,Update val
-                cur.next.key = key
                 return 
             cur = cur.next
         cur.next = ListNode(key)    
 
     def remove(self, key: int) -> None:
-        cur = self.map[self.hash(key)] # here we start from dummy node bc we need to do some pointer manipulation
+        cur = self.set[self.hash(key)] # here we start from dummy node bc we need to do some pointer manipulation
         while cur and cur.next:
             if cur.next.key == key: # Here .next bc I want to stop at the node prior to the node I want to remove
                 cur.next = cur.next.next
@@ -31,12 +30,12 @@ class MyHashSet:
             cur = cur.next   
 
     def contains(self, key: int) -> bool:
-        cur = self.map[self.hash(key)].next # .next bc I don't want to check dummy node value
+        cur = self.set[self.hash(key)].next # .next bc I don't want to check dummy node value
         while cur:
             if cur.key == key :
                 return True
             cur = cur.next
-        return        
+        return False
 
 
 
